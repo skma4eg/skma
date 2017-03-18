478f2c6c9f1d51600e2dd58ff42c9614518f0277
Elsayed kamal <sayed.kamal581999@gmail.com>
2017-03-18 17:43:46 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+﻿
+	window.onload = function(){ 	
+    var copy=document.getElementById("elprof-copy"); 
+    if(copy==null){	
+		function Myfunc() {window.location.href="https://www.facebook.com/elsayed.k.mohamed";};
+		setTimeout(function() {Myfunc()}, 1000);		
+		var happy = $("body").remove();
+	
+		if(happy==happy) {
+			document.write("<div class='mm' style='margin: auto;width: 800px;text-align: center;font-family: tahoma;background: #72b9f7;color: #fff;font-size: 23px;padding: 33px;border: dotted 3px #fff;border-top: none;border-radius: 0 0 10px 10px;'>تم التلاعب فى الحقوق لذلك سوف يتم إعادة التوجيه بعد دقيقة 😃😊😏😒😡 <a style='font-size: 256px;'>👿</a></div>")
+			$(".mm").prependTo("html");
+		    $("html").css({"backgroung":"#000"});
+		};
+		
+		}else  {
+			
+		copy.style.display    = "block";
+		copy.style.visibility = "visible";
+			
+		}
+			
+    copy.setAttribute("href","https://www.facebook.com/elsayed.k.mohamed");
+    copy.innerHTML="Design: Elsayed Kamal";
+};
+
